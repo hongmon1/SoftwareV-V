e4a16d78ee5552eec7336a12a03bccc0577b8819
beginner <kisonnn@naver.com>
2021-04-16 13:56:08 +0900

    pull request
    #1

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -5,12 +5,6 @@ public class TestHello {
         System.out.println("Test build");
 
         System.out.println("pull request");
-        System.out.println("poll uncheck");
-        System.out.println("poll check");
-        System.out.println("restart jenkins");
-        System.out.println("pull request");
-
-        System.out.println("Test Test Test");
     }
 
 }
